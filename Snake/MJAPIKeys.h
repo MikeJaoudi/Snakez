f5d0c21974eda8ec3741315a899454efcfef92ba
Mike Jaoudi <mjaoudi93@gmail.com>
2013-02-13 18:46:20 -0500

    Added API Key File

---
+++ Snake/MJAPIKeys.h
@@ -0,0 +1,13 @@
+//
+//  MJAPIKeys.h
+//  Snake
+//
+//  Created by Mike Jaoudi on 2/13/13.
+//
+//
+
+
+static NSString* TESTFLIGHTKEY = @"";
+static NSString* BLUETOOTHKEY = @"";
+static float MULTIPLAYERVERSION = 0.0f;
+static NSString* BANNERADID = @"";
